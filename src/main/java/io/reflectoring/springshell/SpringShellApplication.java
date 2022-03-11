9ff30c01a769f572358aa4e86ed2ed281e8142f6
gzak001 <100429071+gzak001@users.noreply.github.com>
2022-03-11 13:21:07 -0500

    bbbbbbbbbbbbbbbbbbbbbbbbbbbbb

--- src/main/java/io/reflectoring/springshell/SpringShellApplication.java
+++ src/main/java/io/reflectoring/springshell/SpringShellApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringShellApplication {
+	// Hello
 	public static void main(String[] args) {
 		SpringApplication.run(SpringShellApplication.class, args);
 	}
