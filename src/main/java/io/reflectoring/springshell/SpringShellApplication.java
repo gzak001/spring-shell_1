d84833ae3d468a45e85b0bd14aef709d73b4ccd2
gzak001 <100429071+gzak001@users.noreply.github.com>
2023-07-13 14:33:27 -0400

    Update SpringShellApplication.java

--- src/main/java/io/reflectoring/springshell/SpringShellApplication.java
+++ src/main/java/io/reflectoring/springshell/SpringShellApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringShellApplication {
-	// Hello WORlD 8
+	// Hello WORlD 9
 	public static void main(String[] args) {
 		SpringApplication.run(SpringShellApplication.class, args);
 	}
